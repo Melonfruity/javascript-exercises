48843f0d7fcc577df24a1ee3721e6c993c32f366
melonfruity <eric.luo24@gmail.com>
2019-10-28 22:44:13 -0400

    removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
+  // const args = Array.from(arguments);
+  const args = [...arguments];
+  const rem = args.slice(1);
 
+  return args[0].filter(num => !rem.includes(num))
 }
 
 module.exports = removeFromArray
