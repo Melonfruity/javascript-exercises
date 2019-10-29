a3a0c09953f1315babbd791cd12482544f8f9397
melonfruity <eric.luo24@gmail.com>
2019-10-28 23:18:00 -0400

    tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * 50/9 ) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return Math.round(((celcius * 18 + 320))) / 10;
 }
+
+console.log(ftoc(100))
 
 module.exports = {
   ftoc,
