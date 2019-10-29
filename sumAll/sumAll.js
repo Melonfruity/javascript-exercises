a3744f544842c345b37cea48268efd072b46c2f2
melonfruity <eric.luo24@gmail.com>
2019-10-28 22:56:35 -0400

    sumAll.js want to try doing it recursively too

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  
+  if(typeof start !== 'number' || typeof end !== 'number'){
+    return 'ERROR'
+  } else if(start < 0 || end < 0){
+    return 'ERROR'
+  }
 
-}
+  let sum = 0;
+
+  for(let i = (start < end ? start : end) ; i <= (start > end ? start : end); i ++){
+    sum = sum + i;
+  }
+
+  return sum;
+  
+  /*
+  if((start+1) !== end){
+    return sumAll(start, end) + sumAll(start+1, end);
+  }
+
+  return 
+  */
+} 
 
 module.exports = sumAll
