0ac990c84309b39917326e1bfc4f634d3a49feeb
melonfruity <eric.luo24@gmail.com>
2019-10-28 22:17:28 -0400

    repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
+  
+  if(repeat < 0){
+    return 'ERROR';
+  }
+  
+  let newStr = '';
+  
+  for(let i = 0; i < repeat; i++ ){
+    newStr += str;
+  }
 
+  return newStr;
 }
 
 module.exports = repeatString
