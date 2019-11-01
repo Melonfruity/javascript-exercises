0659292b9256045e4568f84de40ec858492daec8
melonfruity <eric.luo24@gmail.com>
2019-11-01 18:54:12 -0400

    it works for all but normal negative shift, problem is that the lower bound for cap letters isnt addressed

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+  const s = [...str].map((c, index) => {
+    const charCode = str.charCodeAt(index);
+    const newCharCode = str.charCodeAt(index) + (shift % 26);
 
+    if(charCode <= 90 && charCode > 64){
+      return newCharCode > 90 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode);
+    } else if (charCode <= 122 && charCode > 96) {
+      return newCharCode > 122 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode);
+    } else {
+      return c;
+    }
+  });
 }
-
 module.exports = caesar
