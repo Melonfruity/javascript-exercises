dfb971c100e0a57f27505ec5111088386532b847
melonfruity <eric.luo24@gmail.com>
2019-11-01 19:04:49 -0400

    added logic for <65 charcode

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -3,13 +3,24 @@ const caesar = function(str, shift) {
     const charCode = str.charCodeAt(index);
     const newCharCode = str.charCodeAt(index) + (shift % 26);
 
-    if(charCode <= 90 && charCode > 64){
-      return newCharCode > 90 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode);
-    } else if (charCode <= 122 && charCode > 96) {
-      return newCharCode > 122 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode);
-    } else {
+    // 65, 90 | 97, 122
+    if(charCode <= 90 && charCode >= 65){
+      if(newCharCode > 90 || newCharCode < 65){
+        return newCharCode > 90 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode + 26);
+      } else {
+        return String.fromCharCode(newCharCode);
+      }
+    } else if(charCode <= 122 && charCode >= 97){
+      if(newCharCode > 122 || newCharCode < 97){
+        return newCharCode > 122 ? String.fromCharCode(newCharCode - 26) : String.fromCharCode(newCharCode + 26);
+      } else {
+        return String.fromCharCode(newCharCode);
+      }
+    } else 
       return c;
-    }
   });
+
+  return s.join('');
 }
+
 module.exports = caesar
