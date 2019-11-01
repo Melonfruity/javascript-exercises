2fa478a4caa2621e1158e22092d9dd10c97feb8c
melonfruity <eric.luo24@gmail.com>
2019-11-01 17:30:46 -0400

    completed all cal exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,29 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (a) {
+	return a.reduce((total, num) => add(total, num), 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((total, num) => total * num, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let total = 1;
+	for (let i = 1; i <= a; i ++){
+		total *= i;
+	}
+	return total;
 }
 
 module.exports = {
@@ -27,6 +31,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
 }
