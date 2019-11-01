7cff86e2046dd2d6ddd255e522cfb340beff5ad1
melonfruity <eric.luo24@gmail.com>
2019-11-01 17:57:34 -0400

    completed palindromes... need to read up on regex

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const newStr = str.toLowerCase().replace(/\s/g, '').replace(/\W/g, '');
+  return newStr === newStr.split('').reverse().join('');
 }
 
 module.exports = palindromes
