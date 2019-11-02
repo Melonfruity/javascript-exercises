57faeaf0a82303f30024b579879ba259566e6588
melonfruity <eric.luo24@gmail.com>
2019-11-01 20:37:51 -0400

    could make it look nicer...

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
 
+  return people.reduce((oldestPerson, person) => {
+
+    if(!person.yearOfDeath)
+      person.yearOfDeath = 2019;  
+      
+    if(oldestPerson){
+      return oldestPerson.yearOfDeath - oldestPerson.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? oldestPerson : person;
+    } else 
+      return person;
+  }, {})
 }
 
 module.exports = findTheOldest
