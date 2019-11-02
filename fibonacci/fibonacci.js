99d4aa0e04030ed15ad9dc6cbdd0c92f4adc36a3
melonfruity <eric.luo24@gmail.com>
2019-11-01 20:06:08 -0400

    non-recursive fib

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if(!Number(num) || Number(num) < 0 ) return 'OOPS'
+  
+  let a = 0;
+  let b = 1;
 
+  for (let i = 1; i < num; i ++){ 
+    let c = a;
+    a = b;
+    b += c;
+  }
+  return b;
 }
+
+console.log(fibonacci("8"));
 
 module.exports = fibonacci
